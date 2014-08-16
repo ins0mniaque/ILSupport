3e5eb52bd88ecb3ccd4f632d11a28f396bf26ad6
Intas <floatas@outlook.com>
2014-08-17 01:50:32 +0300

    Intellisense update, completion list with descriptions for 224 opcodes

--- IL Support/Intellisense/LanguageCompletionCommandHandler.cs
+++ IL Support/Intellisense/LanguageCompletionCommandHandler.cs
@@ -105,8 +105,10 @@ namespace ILSupport.Intellisense
             {
                 if (m_session == null || m_session.IsDismissed) // If there is no active session, bring up completion
                 {
-                    this.TriggerCompletion();
-                    m_session.Filter();
+                    if (this.TriggerCompletion())
+                    {
+                        m_session.Filter();
+                    }
                 }
                 else     //the completion session is already active, so just filter
                 {
