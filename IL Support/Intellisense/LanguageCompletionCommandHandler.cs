d003d1a9e02b03dff1564162450a3d85795070fd
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2016-06-12 20:16:37 -0400

    Restyled intellisense implementation to match project coding style

--- IL Support/Intellisense/LanguageCompletionCommandHandler.cs
+++ IL Support/Intellisense/LanguageCompletionCommandHandler.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.VisualStudio;
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Runtime.InteropServices;
+
+using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Editor;
 using Microsoft.VisualStudio.Language.Intellisense;
 using Microsoft.VisualStudio.OLE.Interop;
@@ -7,36 +11,30 @@ using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.TextManager.Interop;
 using Microsoft.VisualStudio.Utilities;
-using System;
-using System.ComponentModel.Composition;
-using System.Runtime.InteropServices;
-
 
 namespace ILSupport.Intellisense
 {
-    [Export(typeof(IVsTextViewCreationListener))]
-    [Name("token completion handler")]
-    [ContentType("il")]
-    [TextViewRole(PredefinedTextViewRoles.Editable)]
+    [ Export ( typeof ( IVsTextViewCreationListener ) ),
+      Name ( "Token completion handler" ),
+      ContentType ( "il" ),
+      TextViewRole ( PredefinedTextViewRoles.Editable ) ]
     internal class LanguageCompletionHandlerProvider : IVsTextViewCreationListener
     {
-        [Import]
+        [ Import ]
         internal IVsEditorAdaptersFactoryService AdapterService = null;
-        [Import]
+        [ Import ]
         internal ICompletionBroker CompletionBroker { get; set; }
-        [Import]
+        [ Import ]
         internal SVsServiceProvider ServiceProvider { get; set; }
 
-        public void VsTextViewCreated(IVsTextView textViewAdapter)
+        public void VsTextViewCreated ( IVsTextView textViewAdapter )
         {
-            ITextView textView = AdapterService.GetWpfTextView(textViewAdapter);
-            if (textView == null)
+            var textView = AdapterService.GetWpfTextView ( textViewAdapter );
+            if ( textView == null )
                 return;
 
-            Func<LanguageCompletionCommandHandler> createCommandHandler = delegate () { return new LanguageCompletionCommandHandler(textViewAdapter, textView, this); };
-            textView.Properties.GetOrCreateSingletonProperty(createCommandHandler);
+            textView.Properties.GetOrCreateSingletonProperty ( ( ) => new LanguageCompletionCommandHandler ( textViewAdapter, textView, this ) );
         }
-
     }
 
     internal class LanguageCompletionCommandHandler : IOleCommandTarget
@@ -46,126 +44,116 @@ namespace ILSupport.Intellisense
         private LanguageCompletionHandlerProvider m_provider;
         private ICompletionSession m_session;
 
-        internal LanguageCompletionCommandHandler(IVsTextView textViewAdapter, ITextView textView, LanguageCompletionHandlerProvider provider)
+        internal LanguageCompletionCommandHandler ( IVsTextView textViewAdapter, ITextView textView, LanguageCompletionHandlerProvider provider )
         {
-            this.m_textView = textView;
-            this.m_provider = provider;
+            m_textView = textView;
+            m_provider = provider;
 
-            //add the command to the command chain
-            textViewAdapter.AddCommandFilter(this, out m_nextCommandHandler);
+            // Add the command to the command chain
+            textViewAdapter.AddCommandFilter ( this, out m_nextCommandHandler );
         }
 
-        public int QueryStatus(ref Guid pguidCmdGroup, uint cCmds, OLECMD[] prgCmds, IntPtr pCmdText)
+        public int QueryStatus ( ref Guid pguidCmdGroup, uint cCmds, OLECMD [ ] prgCmds, IntPtr pCmdText )
         {
-            return m_nextCommandHandler.QueryStatus(ref pguidCmdGroup, cCmds, prgCmds, pCmdText);
+            return m_nextCommandHandler.QueryStatus ( ref pguidCmdGroup, cCmds, prgCmds, pCmdText );
         }
 
-        public int Exec(ref Guid pguidCmdGroup, uint nCmdID, uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut)
+        public int Exec ( ref Guid pguidCmdGroup, uint nCmdID, uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut )
         {
-            if (VsShellUtilities.IsInAutomationFunction(m_provider.ServiceProvider))
-            {
-                return m_nextCommandHandler.Exec(ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
-            }
-            //make a copy of this so we can look at it after forwarding some commands 
+            if ( VsShellUtilities.IsInAutomationFunction ( m_provider.ServiceProvider ) )
+                return m_nextCommandHandler.Exec ( ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut );
+
+            // Make a copy of this so we can look at it after forwarding some commands
             uint commandID = nCmdID;
             char typedChar = char.MinValue;
-            //make sure the input is a char before getting it 
-            if (pguidCmdGroup == VSConstants.VSStd2K && nCmdID == (uint)VSConstants.VSStd2KCmdID.TYPECHAR)
-            {
-                typedChar = (char)(ushort)Marshal.GetObjectForNativeVariant(pvaIn);
-            }
+            // Make sure the input is a char before getting it
+            if ( pguidCmdGroup == VSConstants.VSStd2K && nCmdID == (uint) VSConstants.VSStd2KCmdID.TYPECHAR )
+                typedChar = (char) (ushort) Marshal.GetObjectForNativeVariant ( pvaIn );
 
-            //check for a commit character 
-            if (nCmdID == (uint)VSConstants.VSStd2KCmdID.RETURN
-                || nCmdID == (uint)VSConstants.VSStd2KCmdID.TAB
-                || char.IsWhiteSpace(typedChar))
+            // Check for a commit character
+            if ( nCmdID == (uint) VSConstants.VSStd2KCmdID.RETURN ||
+                 nCmdID == (uint) VSConstants.VSStd2KCmdID.TAB    ||
+                 char.IsWhiteSpace ( typedChar ) )
             {
-                //check for a a selection 
-                if (m_session != null && !m_session.IsDismissed)
+                // Check for a a selection
+                if ( m_session != null && ! m_session.IsDismissed )
                 {
-                    //if the selection is fully selected, commit the current session 
-                    if (m_session.SelectedCompletionSet.SelectionStatus.IsSelected)
+                    // If the selection is fully selected, commit the current session
+                    if ( m_session.SelectedCompletionSet.SelectionStatus.IsSelected )
                     {
-                        m_session.Commit();
-                        //also, don't add the character to the buffer 
+                        m_session.Commit ( );
+                        // Also, don't add the character to the buffer
                         return VSConstants.S_OK;
                     }
                     else
                     {
-                        //if there is no selection, dismiss the session
-                        m_session.Dismiss();
+                        // If there is no selection, dismiss the session
+                        m_session.Dismiss ( );
                     }
                 }
             }
 
             //pass along the command so the char is added to the buffer 
-            int retVal = m_nextCommandHandler.Exec(ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
+            int retVal = m_nextCommandHandler.Exec ( ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut );
             bool handled = false;
-            if (!typedChar.Equals(char.MinValue) && (char.IsLetterOrDigit(typedChar) || typedChar == '.'))
+            if ( ! typedChar.Equals ( char.MinValue ) && ( char.IsLetterOrDigit ( typedChar ) || typedChar == '.' ) )
             {
-                if (m_session == null || m_session.IsDismissed) // If there is no active session, bring up completion
+                if ( m_session == null || m_session.IsDismissed ) // If there is no active session, bring up completion
                 {
-                    if (this.TriggerCompletion())
+                    if ( TriggerCompletion ( ) )
                     {
-                        if (m_session != null)
-                        {
-                            m_session.Filter();
-                        }
+                        if ( m_session != null )
+                            m_session.Filter ( );
                     }
                 }
-                else     //the completion session is already active, so just filter
+                else // The completion session is already active, so just filter
                 {
-                    m_session.Filter();
+                    m_session.Filter ( );
                 }
+
                 handled = true;
             }
-            else if (commandID == (uint)VSConstants.VSStd2KCmdID.BACKSPACE   //redo the filter if there is a deletion
-                || commandID == (uint)VSConstants.VSStd2KCmdID.DELETE)
+            else if ( commandID == (uint) VSConstants.VSStd2KCmdID.BACKSPACE || // Redo the filter if there is a deletion
+                      commandID == (uint) VSConstants.VSStd2KCmdID.DELETE )
             {
-                if (m_session != null && !m_session.IsDismissed)
-                    m_session.Filter();
+                if ( m_session != null && ! m_session.IsDismissed )
+                    m_session.Filter ( );
+
                 handled = true;
             }
-            if (handled) return VSConstants.S_OK;
-            return retVal;
+
+            return handled ? VSConstants.S_OK : retVal;
         }
 
-
-        private bool TriggerCompletion()
+        private bool TriggerCompletion ( )
         {
-            //the caret must be in a non-projection location 
-            SnapshotPoint? caretPoint =
-            m_textView.Caret.Position.Point.GetPoint(
-            textBuffer => (!textBuffer.ContentType.IsOfType("projection")), PositionAffinity.Predecessor);
-            if (!caretPoint.HasValue)
-            {
+            // The caret must be in a non-projection location
+            var caretPoint = m_textView.Caret.Position.Point.GetPoint ( textBuffer => ! textBuffer.ContentType.IsOfType ( "projection" ),
+                                                                        PositionAffinity.Predecessor );
+            if ( ! caretPoint.HasValue )
                 return false;
-            }
 
-            m_session = m_provider.CompletionBroker.CreateCompletionSession
-         (m_textView,
-                caretPoint.Value.Snapshot.CreateTrackingPoint(caretPoint.Value.Position, PointTrackingMode.Positive),
-                true);
+            var trackingPoint = caretPoint.Value.Snapshot.CreateTrackingPoint ( caretPoint.Value.Position, PointTrackingMode.Positive );
+            m_session = m_provider.CompletionBroker.CreateCompletionSession ( m_textView, trackingPoint, true );
 
-            //subscribe to the Dismissed event on the session 
-            m_session.Dismissed += this.OnSessionDismissed;
+            // Subscribe to the Dismissed event on the session
+            m_session.Dismissed += OnSessionDismissed;
 
-            m_session.Start();
+            m_session.Start ( );
 
-            //wait to start
-            while (m_session.IsStarted == false && m_session.IsDismissed == false)
+            // Wait to start
+            while ( ! m_session.IsStarted && ! m_session.IsDismissed )
             {
 
             }
 
-            return m_session.IsStarted == false && m_session.IsDismissed == false;
+            return ! m_session.IsStarted && ! m_session.IsDismissed;
         }
 
-        private void OnSessionDismissed(object sender, EventArgs e)
+        private void OnSessionDismissed ( object sender, EventArgs e )
         {
-            m_session.Dismissed -= this.OnSessionDismissed;
+            m_session.Dismissed -= OnSessionDismissed;
             m_session = null;
         }
-
     }
 }
--- IL Support/Intellisense/LanguageCompletionSourceProvider.cs
+++ IL Support/Intellisense/LanguageCompletionSourceProvider.cs
@@ -1,28 +1,23 @@
-﻿using Microsoft.VisualStudio.Language.Intellisense;
+﻿using System.ComponentModel.Composition;
+
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Operations;
 using Microsoft.VisualStudio.Utilities;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ILSupport.Intellisense
 {
-    [Export(typeof(ICompletionSourceProvider))]
-    [ContentType("il")]
-    [Name("token completion")]
-
-    internal class LanguageCompletionSourceProvider :ICompletionSourceProvider
+    [ Export ( typeof ( ICompletionSourceProvider ) ),
+      Name ( "Token completion" ),
+      ContentType ( "il" ) ]
+    internal class LanguageCompletionSourceProvider : ICompletionSourceProvider
     {
-        [Import]
+        [ Import ]
         internal ITextStructureNavigatorSelectorService NavigatorService { get; set; }
 
-
-        public ICompletionSource TryCreateCompletionSource(Microsoft.VisualStudio.Text.ITextBuffer textBuffer)
+        public ICompletionSource TryCreateCompletionSource ( ITextBuffer textBuffer )
         {
-            return new LanguageCompletionSource(this, textBuffer);
+            return new LanguageCompletionSource ( this, textBuffer );
         }
     }
 }
