cafbe80d42743e418ff72fb32b8d74ce6d5d3aa9
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2017-06-30 12:55:01 -0400

    Removed dependency on Microsoft.VisualStudio.Shell because version 14 is not shipped with Visual Studio 2017 and version 15 requires .NET 4.6.2

---
+++ IL Support/Intellisense/Microsoft.VisualStudio.Shell.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using EnvDTE;
+
+namespace ILSupport.Intellisense
+{
+    internal class VSConstants
+    {
+        public const int S_OK = 0;
+
+        public static readonly Guid VSStd2K = new Guid ( "{1496A755-94DE-11D0-8C3F-00C04FC2AAE2}" );
+
+        public enum VSStd2KCmdID
+        {
+            TYPECHAR = 1,
+            BACKSPACE,
+            RETURN,
+            TAB,
+            BACKTAB,
+            DELETE
+        }
+    }
+
+    internal class VsShellUtilities
+    {
+        public static bool IsInAutomationFunction ( IServiceProvider serviceProvider )
+        {
+            if ( serviceProvider == null )
+                throw new ArgumentException ( "serviceProvider" );
+
+            var vsExtensibility = serviceProvider.GetService ( typeof ( IVsExtensibility ) ) as IVsExtensibility;
+            if ( vsExtensibility == null )
+                throw new InvalidOperationException ( );
+
+            return vsExtensibility.IsInAutomationFunction ( ) != 0;
+        }
+    }
+}
