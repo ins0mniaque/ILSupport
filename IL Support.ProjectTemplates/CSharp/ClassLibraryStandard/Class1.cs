ad1d2bbe6cd0f708e50b2912afab1bf0c0d2d2c6
Jeff McCormick <jeffreym@dmcinfo.com>
2018-06-05 14:20:29 -0400

    Add new templates, targets, and source files for new .NET Standard and .NET Core Class Library project options

---
+++ IL Support.ProjectTemplates/CSharp/ClassLibraryStandard/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace $safeprojectname$
+{
+	public class Class1
+	{
+		[MethodImpl(MethodImplOptions.ForwardRef)]
+		public extern int Square(int number);
+	}
+}
